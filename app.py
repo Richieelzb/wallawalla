7f147933e5d2c891752592141e124c7a2b5c9a32
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-09-22 02:04:46 +0200

    create

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
  return render_template("frontend.html")
+
+@app.route("/health")
+def health():
+   return {"status": "healthy"}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
